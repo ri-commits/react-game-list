cc05e02fe6c2e1a4d1beae4ba35b44e3dbadf82f
ri-commits <kartashourychard@gmail.com>
2023-04-22 08:40:55 +0200

    state exarsise

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,39 @@ import ListGroup from "./components/ListGroup";
 import "./App.css";
 import { BsCalendar2DateFill } from "react-icons/bs";
 import Like from "./components/like/Like";
+import Message from "./Message";
 
 export const App = () => {
   let items = ["New York", "San Francisco", "Tokyo", "Seul"];
   const [alertVisible, setAlertVisibility] = useState(false);
+  const [isApproved, setApproved] = useState(true);
+
+  const [person, setPerson] = useState({
+    firstName: "",
+    lastName: "",
+  });
 
   const handleSelectItem = (item: string) => {
     console.log(item);
+    console.log(alertVisible);
+  };
+
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
+
+  const handleClick = () => {
+    setDrink({ ...drink, price: 6 });
   };
 
   return (
     <div>
+      <div>
+        {drink.price}
+        <button onClick={handleClick}>Click Me</button>
+      </div>
+      <Message />
       <Like onClick={() => console.log("clicked")} />
       <BsCalendar2DateFill color="red" size="40" />
       {alertVisible && (
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,7 +1,8 @@
 function Message() {
-  const name = "";
-  if (name) return <h1>Hello {name}</h1>;
-  return <h1>Hello World</h1>;
+  let count = 0;
+  console.log("Message called", count);
+  count++;
+  return <div>Message {count}</div>;
 }
 
 export default Message;
